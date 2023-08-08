6d9a22b6674fdc8f68f06419cd70589130278924
Proxemy <33573115+proxemy@users.noreply.github.com>
2023-08-08 13:34:00 +0200

    Example 1 and 2 work.

--- docker1.nix
+++ docker1.nix
@@ -1,17 +1,24 @@
-#TODO:
 # * Cmd "-e" parameter needs a persistent/secure location to store logs to
 
 { pkgs ? import <nixpkgs> {} }:
+let
+	nginxConf = pkgs.writeText "nginx.conf" ''
+		user nobody nobody;
+		pid /dev/null;
+		events {}
+	'';
+in
 pkgs.dockerTools.buildImage
 {
 	name = "docker-test";
-	copyToRoot = [ pkgs.nginx pkgs.coreutils ]; #pkgs.fakeNss
+	copyToRoot = [ pkgs.nginx pkgs.fakeNss ];
 
 	created = "now";
 
 	config = {
 		Cmd = [
 			"${pkgs.nginx}/bin/nginx"
+			"-c" nginxConf
 			"-e" "/tmp"
 		];
 	};
--- run.sh
+++ run.sh
@@ -3,9 +3,13 @@
 set -xeuo pipefail
 
 pidof dockerd || exit 1
+mkdir -p /tmp/nginx_client_body
 
 nix_result=$(nix-build docker"$1".nix)
 
 docker_image=$(docker load < "$nix_result" | cut -d' ' -f3)
 
-docker run "$docker_image"
+docker run -p 80:80 "$docker_image"
+#docker run "$docker_image"
+# shell into into runnig container
+#docker run -it --entrypoint /bin/bash "$docker_image"
