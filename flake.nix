9f40b4ea0f009fb5e5b83c7084ce4e41f2f59061
Proxemy <33573115+proxemy@users.noreply.github.com>
2023-08-19 15:17:20 +0200

    Added bare minimum flake.nix

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+	description = "Bare minimum example flake";
+
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+	};
+
+	outputs = { self, nixpkgs }:
+	let
+		system = "x86_64-linux";
+		pkgs = nixpkgs.legacyPackages.${system};
+	in
+	{
+		packages.${system}.default = pkgs.apacheHttpd;
+	};
+}
