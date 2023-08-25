bc95aad01fc0d5606981653ed74252341f1c7530
Proxemy <33573115+proxemy@users.noreply.github.com>
2023-08-25 23:32:31 +0200

    Added minimal postgresql.conf file. Its running now! Even though, the config file is incomplete.

--- flake.nix
+++ flake.nix
@@ -58,9 +58,31 @@ in
 		}
 	'';
 
-	postgres-cfg = {}:
-	pkgs.writeTextDir "postgresql.conf" ''
-		listen_addresses = 'localhost'
+	postgresql-cfg = { port ? "5432" }:
+	pkgs.writeText "postgreql.conf" ''
+			#datestyle = 'iso, mdy'
+			timezone = 'America/Chicago'
+			lc_messages = 'C'			# locale for system error message strings
+			lc_monetary = 'C'			# locale for monetary formatting
+			lc_numeric = 'C'			# locale for number formatting
+			lc_time = 'C'				# locale for time formatting
+
+			# default configuration for text search
+			listen_addresses = '*'		# what IP address(es) to listen on;
+			port = ${port}
+			max_connections = 100			# (change requires restart)
+			unix_socket_directories = '/tmp'	# comma-separated list of directories
+			shared_buffers = 128MB			# min 128kB
+			dynamic_shared_memory_type = posix	# the default is the first option
+			max_wal_size = 1GB
+			min_wal_size = 80MB
+
+			log_timezone = 'America/Chicago'
+			default_text_search_config = 'pg_catalog.english'
+
+			# TODO: figure out, wth this is
+			#shared_preload_libraries = 'auto_explain,pgsodium'
+			#pgsodium.getkey_script = '@PGSODIUM_GETKEY_SCRIPT@'
 	'';
 
 
@@ -136,14 +158,13 @@ in
 				pathsToLink = [ pkgs.postgresql "/etc" ]; # /etc{nsswitch.conf,passwd} is required by nginx/getpwnam()
 			};
 
-			#extraCommands = "cp -p ./postgresql.conf $out/etc/";
-
 			config = {
 				User = "nobody:nobody";
 				Cmd =
 				[
 					"${pkgs.postgresql}/bin/postgres"
-					"--config-file" "TODO"
+					"-d" (if globalDebug then "5" else "1")
+					"--config-file=${self.postgresql-cfg {} }"
 				];
 			};
 		};
