46e1145d04e15d8fe42892a7aa4fd991f95757f5
Proxemy <33573115+proxemy@users.noreply.github.com>
2023-08-27 17:51:12 +0200

    Added docker volume creation/cleanup.

--- run.sh
+++ run.sh
@@ -10,6 +10,8 @@ docker stop   $(docker ps -aq) || true
 if [[ "$@" =~ ^-c$ ]]; then
 	docker rm -vf $(docker ps -aq) || true
 	docker rmi -f $(docker images -aq) || true
+	docker volume rm $(docker volume ls -q) || true
+	exit 0
 fi
 
 if [ -z ${1-""} ] || [[ "$@" =~ ^- ]]; then
@@ -24,9 +26,10 @@ nix_result=$(readlink result)
 docker_image=$(docker load < "$nix_result" | cut -d' ' -f3)
 
 if [[ "$@" =~ ^-i$ ]]; then
-	docker run -it "$docker_image" sh
+	docker run -it -v data:/data "$docker_image" sh
 else
 	# docker volume create data
 	#docker run -p 80:80 --mount type=volume,src=data,target=/data "$docker_image"
-	docker run -p 80:80 -v data:/data "$docker_image"
+	#docker run -v data:/data "$docker_image"
+	docker run -v data:/data "$docker_image"
 fi
