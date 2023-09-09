dbcac171331ea8f8d28d2a74530993768523bc65
Proxemy <33573115+proxemy@users.noreply.github.com>
2023-09-09 13:06:13 +0200

    Refactored the run script.

--- run.sh
+++ run.sh
@@ -2,34 +2,52 @@
 
 set -xeuo pipefail
 
-pidof dockerd || exit 1
 
-# stopping and removing all previous docker container/images
-docker stop   $(docker ps -aq) || true
+nix_build_args="--debug"
+docker_run_args=""
 
-if [[ "$@" =~ ^-c$ ]]; then
+
+load_docker_result()
+{
+	nix_result=$(readlink result)
+	docker load < "$nix_result" | cut -d' ' -f3
+}
+
+reset_docker()
+{
 	docker rm -vf $(docker ps -aq) || true
 	docker rmi -f $(docker images -aq) || true
 	docker volume rm $(docker volume ls -q) || true
-	exit 0
-fi
+}
 
-if [ -z ${1-""} ] || [[ "$@" =~ ^- ]]; then
-	nix build .#docker-www --debug
-	#nix build .#docker-db --debug
-else
-	nix build .#"$1" --debug
-fi
+launch_db()
+{
+	nix build .#docker-db "$nix_build_args"
+	docker run -v data:/data "$(load_docker_result)"
+}
 
-nix_result=$(readlink result)
+launch_www()
+{
+	nix build .#docker-www "$nix_build_args"
+	docker run -p 80:80 "$(load_docker_result)"
+}
 
-docker_image=$(docker load < "$nix_result" | cut -d' ' -f3)
 
-if [[ "$@" =~ ^-i$ ]]; then
-	docker run -it -v data:/data "$docker_image" sh
-else
-	# docker volume create data
-	#docker run -p 80:80 --mount type=volume,src=data,target=/data "$docker_image"
-	#docker run -v data:/data "$docker_image"
-	docker run -p 80:80 -v data:/data "$docker_image"
-fi
+
+pidof dockerd || exit 1
+
+# stopping and removing all previous docker container/images
+docker stop $(docker ps -aq) || true
+
+for arg in "$@"; do
+	case "$arg" in
+		"-c") reset_docker ;;
+		"-i") docker_run_args+="-it sh" ;;
+		"db") launch_db ;;
+		"www") launch_www ;;
+	esac
+done
+
+# docker volume create data
+#docker run -p 80:80 --mount type=volume,src=data,target=/data "$docker_image"
+#docker run -v data:/data "$docker_image"
